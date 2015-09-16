b8f8902b2dbf578f7233428de5f84e91a14607f0
bmt <brian.m.turnbull@gmail.com>
2015-09-16 08:17:13 -0500

    Remove unnecessary config and ui bits.

--- frontend/config/config.js
+++ frontend/config/config.js
@@ -4,29 +4,15 @@
  * Module dependencies.
  */
 var _ = require('lodash'),
-	glob = require('glob'),
-	fs = require('fs');
-
-/**
- * Resolve environment configuration by extending each env configuration file,
- * and lastly merge/override that with any local repository configuration that exists
- * in local.js
- */
-var resolvingConfig = function() {
-	var conf = {};
-
-	conf = _.extend(
-		require('./env/all'),
-		require('./env/' + process.env.NODE_ENV) || {}
-	);
-
-	return _.merge(conf, (fs.existsSync('./config/env/local.js') && require('./env/local.js')) || {});
-};
+	glob = require('glob');
 
 /**
  * Load app configurations
  */
-module.exports = resolvingConfig();
+module.exports = _.extend(
+  require('./env/all'),
+  require('./env/' + process.env.NODE_ENV) || {}
+);
 
 /**
  * Get files by glob patterns
