c9cd69c32899c8d81ab46ffcd14f555b1170cce2
Brian Turnbull <brian.m.turnbull@gmail.com>
2015-10-05 02:36:05 +0000

    Update interface setup script to correct arch string for pi2.

--- interface/setup.sh
+++ interface/setup.sh
@@ -5,11 +5,14 @@ if [[ $(uname) == "Darwin" ]]; then
   echo "Downloading OpenFrameworks 0.8.4 for mac."
   curl -O http://www.openframeworks.cc/versions/v0.8.4/of_v0.8.4_osx_release.zip
   unzip of_v0.8.4_osx_release.zip
-elif [[ $(uname -m) == "arm7l" ]]; then
+elif [[ $(uname -m) == "armv7l" ]]; then
   echo "Downloading OpenFrameworks 20151003 nightly for Pi2"
   cd ../
   curl -O http://192.237.185.151/versions/nightly/of_v20151003_linuxarmv7l_nightly.tar.gz
   tar xvzf of_v20151003_linuxarmv7l_nightly.tar.gz
+else
+  echo "Unrecognized machine type: "
+  echo $(uname -a)
 fi
 
 cd -
