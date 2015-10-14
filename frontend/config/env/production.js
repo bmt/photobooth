ac31ec244c0d1a5f396ca4cf73d944f9930c2030
Brian Turnbull <brian.m.turnbull@gmail.com>
2015-10-14 13:41:15 -0500

    Remove incorrect prod asset definition.

--- frontend/config/env/production.js
+++ frontend/config/env/production.js
@@ -17,24 +17,6 @@ module.exports = {
 			stream: 'access.log'
 		}
 	},
-	assets: {
-		lib: {
-			css: [
-				'public/lib/bootstrap/dist/css/bootstrap.min.css',
-				'public/lib/bootstrap/dist/css/bootstrap-theme.min.css',
-			],
-			js: [
-				'public/lib/angular/angular.min.js',
-				'public/lib/angular-resource/angular-resource.min.js',
-				'public/lib/angular-animate/angular-animate.min.js',
-				'public/lib/angular-ui-router/release/angular-ui-router.min.js',
-				'public/lib/angular-ui-utils/ui-utils.min.js',
-				'public/lib/angular-bootstrap/ui-bootstrap-tpls.min.js'
-			]
-		},
-		css: 'public/dist/application.min.css',
-		js: 'public/dist/application.min.js'
-	},
 	google: {
 		clientID: process.env.GOOGLE_ID || 'APP_ID',
 		clientSecret: process.env.GOOGLE_SECRET || 'APP_SECRET',
